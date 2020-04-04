d155144b49cba7204afe7b47b1c6de886ac52589
fahimeh <fahimeh.fakhri@physics.sharif.edu>
2020-04-04 21:56:21 +0430

    the main function for extracting metadata from arxiv.org

---
+++ main.py
@@ -0,0 +1,7 @@
+import urllib.request as libreq
+
+def main():
+    path = input("Enter the path(example --> D:/MachineLearning/) --> ")
+    choosing_variable = input("which one?\n1.title\n2.Author\n3.Abstract\n4.Comment\n5.Journal Reference\n6.Subject Category\n7.Report Number\n8.Id\n9.All of the above\n10.time\n(example --> 1+2+7)\n --> ")
+    return
+main()
