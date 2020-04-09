0f7e6087693a34e3b65c60b75d91a6f17e1143ce
fahimeh <fahimeh.fakhri@physics.sharif.edu>
2020-04-10 00:44:29 +0430

    some little changes

--- json_to_DataFrame.py
+++ json_to_DataFrame.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-path = input("Enter the path of the json file -->")
+path = input("Enter the path of the json file(example --> D:/Machine Learning/data.json) -->")
 
 r = pd.read_json(path)
-r
